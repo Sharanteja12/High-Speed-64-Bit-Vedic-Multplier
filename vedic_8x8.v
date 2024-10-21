785e414b690ffae6b13ee6c71a4377f1a7592f4c
Sharanteja12 <129510680+Sharanteja12@users.noreply.github.com>
2024-10-21 20:01:46 +0530

    Update vedic_8x8.v

--- vedic_8x8.v
+++ vedic_8x8.v
@@ -9,18 +9,21 @@ vedic_4x4 v42(a[7:4],b[7:4],s4);
 vedic_4x4 v43(a[3:0],b[7:4],s2);
 vedic_4x4 v44(a[7:4],b[3:0],s3);
   
-rca_8 a1(s2,s3,s5,0,cout1);
+wire[7:0] k1,k2,k3;
 
-  assign s6 = {4'b0000,s1[7:4]}; // concetanation
+assign k1 = {s4[3:0],s1[7:4]};
+assign k2 = {s3[7:4],s2[3:0]};
+assign k3 = {s2[7:4],s3[3:0]};
+wire [8:0] cs;
 
-rca_8 a2(s5,s6,s7,0,cout2);
+  csa_8 ff1(k1,k2,k3,cs,cout); // 8 Bit Carry Save Adder 
+wire [3:0] m;
+wire c9;
 
-  assign s8 = {3'b000,(cout2|cout1),s7[7:4]}; // concetanation
-
-rca_8 a3(s4,s8,s9,0,cout3);
+rca ff2(s4[7:4],{2'b00,cout,cs[8]},m,0,c9);
 
 assign y[3:0] = s1[3:0];
-assign y[7:4] = s7[3:0];
-assign y[15:8] = s9[7:0];
+assign y[11:4] = cs[7:0];
+assign y[15:12] = m[3:0];
 
 endmodule
